8a022d8721ab2bc1fbf277056c8533105fa90c7d
Scott Densmore <scottdensmore@mac.com>
2013-06-20 13:59:55 -0700

    Update to latest Xcode and ad Adhoc scheme shared

--- tag.sh
+++ tag.sh
@@ -1,11 +1,19 @@
 #!/bin/sh
-tag=$1
-if [ "$tag" == "" ]; then
-	echo "No tag specified"
+branch=$1
+tag=$2
+if [ "$branch"]; then
+	echo "No branch specified"
 	exit
 fi
-agvtool next-version -all
-git commit -a -m "Increment CFBundleVersion for $tag"
+
+if [ "$tag" == "" ]; then
+	tag=( $(xcrun agvtool mvers -terse1) )
+	echo "No tag specified so using current marketing version $tag"
+else 
+	xcrun agvtool new-marketing-version $tag
+fi
+#agvtool next-version -all
+git commit -a -m "Create tag for version $tag"
 git tag -m "Tag for $tag" -a $tag
-git push origin master
+git push origin $branch
 git push --tags
